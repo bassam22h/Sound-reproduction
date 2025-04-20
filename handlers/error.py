7e829f7c6000fc28fbfd377768b2d2375e27bd7b
bassam22h <bassamhamid22@gmail.com>
2025-04-21 00:03:59 +0300

    Update error.py

--- handlers/error.py
+++ handlers/error.py
@@ -1,1 +1,19 @@
+import logging
+from telegram import Update
+from telegram.ext import CallbackContext
+from templates.messages import ERROR_MESSAGE
 
+logger = logging.getLogger(__name__)
+
+async def error_handler(update: Update, context: CallbackContext):
+    try:
+        logger.error(msg="حدث خطأ في المعالج", exc_info=context.error)
+        
+        if update and update.effective_chat:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=ERROR_MESSAGE,
+                parse_mode='Markdown'
+            )
+    except Exception as e:
+        logger.error(f"Error in error handler: {e}")
