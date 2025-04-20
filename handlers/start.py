3afa53626460e24c720bab68eaa2152b983f4586
bassam22h <bassamhamid22@gmail.com>
2025-04-21 01:34:29 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -12,7 +12,7 @@ async def start(update: Update, context: CallbackContext):
         user_name=user.first_name,
         trials=trials,
         max_chars=os.getenv('MAX_CHARS_PER_TRIAL', 100),
-        channels=", ".join([f"@{c}" for c in os.getenv('REQUIRED_CHANNELS', '').split(',') if c])
+        channels=", ".join([f"@{c}" for c in os.getenv('REQUIRED_CHANNELS', '').split(',') if c]))
     
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
