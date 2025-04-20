ff2687e6a77db40f2ba2553981f2fce8fa8ba25b
bassam22h <bassamhamid22@gmail.com>
2025-04-21 00:03:33 +0300

    Update init.py

--- handlers/init.py
+++ handlers/init.py
@@ -1,1 +1,6 @@
+from .start import start
+from .audio import handle_audio
+from .text import handle_text
+from .error import error_handler
 
+__all__ = ['start', 'handle_audio', 'handle_text', 'error_handler']
