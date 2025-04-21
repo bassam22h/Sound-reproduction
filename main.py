7990b1d8a1047b8dac1cab12ef0af0639cccd235
bassam22h <bassamhamid22@gmail.com>
2025-04-21 18:37:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -346,7 +346,7 @@ def run(self):
         port=port,
         debug=False  # تأكد أن debug=False في البيئة الإنتاجية
     )
-
+app = Flask(__name__)
 if __name__ == '__main__':
-    bot = VoiceCloneBot()
-    bot.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
