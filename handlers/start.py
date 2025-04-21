8f8fd91271b8e99f8cbf7155a77dd276669f6927
bassam22h <bassamhamid22@gmail.com>
2025-04-21 03:04:06 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,7 @@
-import os
 from telegram import Update
 from telegram.ext import CallbackContext
 from templates.messages import WELCOME_MESSAGE
-from templates.keyboards import get_main_keyboard
+import os
 
 async def start(update: Update, context: CallbackContext):
     user = update.effective_user
@@ -12,11 +11,10 @@ async def start(update: Update, context: CallbackContext):
         user_name=user.first_name,
         trials=trials,
         max_chars=os.getenv('MAX_CHARS_PER_TRIAL', 100),
-        channels=", ".join([f"@{c}" for c in os.getenv('REQUIRED_CHANNELS', '').split(',') if c]))
+        channels=", ".join([f"@{c}" for c in os.getenv('REQUIRED_CHANNELS', '').split(',') if c])
     
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=welcome_msg,
-        parse_mode='Markdown',
-        reply_markup=get_main_keyboard()
+        parse_mode='Markdown'
     )
