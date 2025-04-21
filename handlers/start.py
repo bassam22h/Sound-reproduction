e8a704cee5d7bb8c4c29532e020f4627b2035fa1
bassam22h <bassamhamid22@gmail.com>
2025-04-21 03:18:43 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,17 +3,20 @@ from telegram.ext import CallbackContext
 from templates.messages import WELCOME_MESSAGE
 import os
 
-async def start(update: Update, context: CallbackContext):
+def start(update: Update, context: CallbackContext):
     user = update.effective_user
     trials = int(os.getenv('DEFAULT_TRIALS', 2))
+    max_chars = os.getenv('MAX_CHARS_PER_TRIAL', 100)
+    channels = ", ".join([f"@{c}" for c in os.getenv('REQUIRED_CHANNELS', '').split(',') if c])
     
     welcome_msg = WELCOME_MESSAGE.format(
         user_name=user.first_name,
         trials=trials,
-        max_chars=os.getenv('MAX_CHARS_PER_TRIAL', 100),
-        channels=", ".join([f"@{c}" for c in os.getenv('REQUIRED_CHANNELS', '').split(',') if c])
+        max_chars=max_chars,
+        channels=channels
+    )
     
-    await context.bot.send_message(
+    context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=welcome_msg,
         parse_mode='Markdown'
