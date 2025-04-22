ec6591e44769e63ba6be765ad788d9259253754c
bassam22h <bassamhamid22@gmail.com>
2025-04-22 20:34:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -550,7 +550,11 @@ def webhook():
         return jsonify({'status': 'error'}), 500
 
 # --- تشغيل التطبيق ---
+app = initialize_bot()
+
+def create_app():
+    return app
+
 if __name__ == '__main__':
-    app = initialize_bot()
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
